4d7d4ce1d60891ef51bb166c735c33a6b4b37fab
Till <tbrodbeck@uos.de>
2020-11-03 12:07:54 +0100

    filtered tokenization outputs to match the maximum length of the model

--- dataProvider/main.py
+++ dataProvider/main.py
@@ -6,25 +6,27 @@ from typing import Dict, List
 from .gerneral_io_utils import read_single_txt, write_txt
 
 DATASET_NAMES = ['golem', 'xsum', 'cnn/dailymail']
+MODEL_NAMES = ['t5-base']
 SPLIT_NAMES = ['train', 'val', 'test']
 TOKENIZER_NAMES = ['WikinewsSum/t5-base-multi-de-wiki-news']
 TEXT_NAMES = ['source', 'target']
 
-def provideData(datasetName: str, tokenizerName: str, size: int = None, createSplits: Dict = None, splits2tokenize: List = SPLIT_NAMES):
+def provideData(datasetName: str, tokenizerName: str, modelName: str, size: int = None, createSplits: Dict = None, splits2tokenize: List = SPLIT_NAMES):
   """Provides tokenized data for training
-
   Args:
     datasetName (str)
     tokenizerName (str)
+    modelName (str)
     size (int, optional): Defaults to None.
     createSplits (Dict, optional): Split the dataset into train, validation and test splits. Defaults to None.
     splits2tokenize (List, optional): Can be set to only tokenize certain splits. Defaults to SPLIT_NAMES.
-
   Raises:
     ValueError: incorrect inputs"""
   # check input
   if not datasetName in DATASET_NAMES:
     raise ValueError('unkown dataset')
+  if not modelName in MODEL_NAMES:
+    raise ValueError('unkown model')
   if not tokenizerName in TOKENIZER_NAMES:
     raise ValueError('unkown tokenizer')
   if size and size < 1:
@@ -54,20 +56,37 @@ def provideData(datasetName: str, tokenizerName: str, size: int = None, createSp
         previousSplitIndex = splitIndex
       assert previousSplitIndex == entries, f'{previousSplitIndex} != {entries}'
 
-  for textName in TEXT_NAMES:  # tokenize
-   for splitName in splits2tokenize:
-    text = read_single_txt('{}{}.{}'.format(dataDir, splitName, textName))
+  # tokenize
+  tokenizer = transformers.AutoTokenizer.from_pretrained(tokenizerName)
+  maxTokenSize = tokenizer.max_model_input_sizes[modelName]
+  for splitName in splits2tokenize:
+    textDeleteMask = []
+    sourceText = read_single_txt('{}{}.{}'.format(dataDir, splitName, 'source'))
+    targetText = read_single_txt('{}{}.{}'.format(dataDir, splitName, 'target'))
     if size:
-      text = text[:size]
-    tokenizer = transformers.AutoTokenizer.from_pretrained(tokenizerName)
-    log(f'creating {textName} batch for {splitName}')
-    pt_batch = tokenizer(
-      text,
-      padding=True,
-      truncation=True,
-      return_tensors="pt"
-    )
-    torch.save(pt_batch, '{}{}_{}.pt'.format(dataDir, splitName, textName))
+      sourceText = sourceText[:size]
+      targetText = targetText[:size]
+    log(f'tokenizing target batch for {splitName}')
+    targetTokens = tokenizer(targetText, padding=True)
+    if len(targetTokens['attention_mask'][0]) > maxTokenSize:
+      targetTokens = len(targetTokens['attention_mask'][0])
+      raise IOError(f'target contains more than {maxTokenSize} tokens: {targetTokens}')
+    log(f'tokenizing source batch for {splitName}')
+    sourceTokens = tokenizer(sourceText, padding=True, max_length=maxTokenSize + 1)
+    for i, attention in enumerate(sourceTokens['attention_mask']):
+      if len(attention) < maxTokenSize:
+        break
+      if attention[maxTokenSize]:
+        textDeleteMask.append(i)
+    for textName, tokens in [('source', sourceTokens), ('target', targetTokens)]:
+      shortTokens = {}
+      for key in tokens:
+        tokensList = tokens[key]
+        for i in sorted(textDeleteMask, reverse=True):
+          del tokensList[i]
+        tokensTensor = torch.LongTensor(tokensList[:maxTokenSize])
+        shortTokens[key] = tokensTensor
+      torch.save(shortTokens, '{}{}_{}.pt'.format(dataDir, splitName, textName))
 
 if __name__ == "__main__":
   fire.Fire(provideData)
