10f9288405cc2979c8831cf84f26fdd35b72ddd1
Till <tbrodbeck@uos.de>
2020-12-02 09:43:15 +0100

    implemented creating multiple checkpoint overview generation

---
+++ evaluator/eval_util.py
@@ -0,0 +1,27 @@
+import os
+import yaml
+
+class Model_Info_Reader():
+  def __init__(self, run_path):
+    self.language, self.model_name, self.run_name = self.read_model_info(run_path)
+
+  def find_model_info_dir(self, model_dir: str) -> str:
+      model_dir_name = os.path.basename(model_dir)
+      if 'checkpoint' in model_dir_name:
+          model_info_dir = os.path.dirname(model_dir)
+      else:
+          model_info_dir = model_dir
+      return model_info_dir
+
+  def find_model_info(self, model_dir: str) -> str:
+      model_info_dir = self.find_model_info_dir(model_dir)
+      model_info_path = model_info_dir + '/model_info.yml'
+      assert os.path.exists(model_info_path)
+      return model_info_path
+
+  def read_model_info(self, model_dir: str):
+      model_info_path = self.find_model_info(model_dir)
+      with open(model_info_path) as f:
+          fileContent = f.read()
+          model_info = yaml.load(fileContent)
+      return model_info['language'], model_info['model_name'], model_info['run_name']
--- tests/evaluator_test.py
+++ tests/evaluator_test.py
@@ -1,9 +1,24 @@
 import environs
-import evaluator.main as o
+from evaluator.eval_util import Model_Info_Reader
+from timelogging.timeLog import log
 import unittest
 
-class TestDataProvider(unittest.TestCase):
-  def test_provideData(self):
+class Test_Model_Info_Reader(unittest.TestCase):
+  def __init__(self, *args, **kwargs):
+    super(Test_Model_Info_Reader, self).__init__(*args, **kwargs)
     env = environs.Env()
     env.read_env()
-    o.evaluate(env('oneDriveDir') + 'studyProjectLuca/results/t5-de-new/long_run')
+    self.run_path = env('oneDriveDir') + 'studyProjectLuca/results/t5-de/0'
+    self.run_path_with_checkpoint = self.run_path + '/checkpoint-5000'
+    self.model_info_reader = Model_Info_Reader(self.run_path)
+    self.model_info_reader_with_checkpoints = Model_Info_Reader(self.run_path_with_checkpoint)
+
+
+  def test_read_model_info(self):
+    assert type(self.model_info_reader.language) == str
+    assert type(self.model_info_reader.model_name) == str
+    assert type(self.model_info_reader.run_name) == str
+
+    assert self.model_info_reader.language == self.model_info_reader_with_checkpoints.language
+    assert self.model_info_reader.model_name == self.model_info_reader_with_checkpoints.model_name
+    assert self.model_info_reader.run_name == self.model_info_reader_with_checkpoints.run_name
