55584b32a564449671b4c5929c9255a31b1b3dd1
Luca Kretz <luca.kretz@ibm.com>
2020-12-05 12:12:06 +0100

    refactoring part 1

--- dataProvider/main.py
+++ dataProvider/main.py
@@ -1,115 +1,160 @@
+"""
+module to read dataset and bring it
+in the right format for training
+"""
+from utilities.gerneral_io_utils import read_single_txt, write_txt, \
+  assertDirExistent, assertFileInxestent, check_make_dir
+from typing import Optional
+import transformers
+from timelogging.timeLog import log
+import torch
+import numpy as np
+import fire
 import sys
 sys.path.append(".")
-import fire
-import numpy as np
-import torch
-from timelogging.timeLog import log
-import transformers
-import typing
-from utilities.gerneral_io_utils import read_single_txt, write_txt, assertDirExistent, assertFileInxestent, check_make_dir
 
 MODEL_NAMES = ['t5-base']
 SPLIT_NAMES = ['train', 'val', 'test']
 TOKENIZER_NAMES = ['WikinewsSum/t5-base-multi-de-wiki-news']
 
-def provideData(datasetName: str, tokenizerName: str, modelName: str, size: int = None, createSplits=False, splits2tokenize: typing.List = SPLIT_NAMES, filtering=True):
-  """Provides tokenized data for training
-  Args:
-    datasetName (str)
-    tokenizerName (str)
-    modelName (str)
-    size (int, optional): Limits the amount of samples that are taken for tokenization for each split. Defaults to None.
-    createSplits (Dict or bool, optional): Split the dataset into train, validation and test splits. Has to be provided as a dict containing the keys `train` and `val` and values between 0 and 1. If `True` uses a default 80/10/10 split. Defaults to False.
-    splits2tokenize (List, optional): Can be set to only tokenize certain splits. Defaults to SPLIT_NAMES.
-    filtering (bool, optional): Longer examples than the maximum token size are filtered, else they are truncated. Defaults to True.
-  Raises:
-    ValueError: incorrect inputs"""
-  # checking input
-  if not modelName in MODEL_NAMES:
-    raise ValueError('unkown model')
-  if not tokenizerName in TOKENIZER_NAMES:
-    raise ValueError('unkown tokenizer')
-  if size and size < 1:
-    raise ValueError('wrong size')
-  dataDir = f'dataProvider/datasets/{datasetName}/'
-  assertDirExistent(dataDir)
 
-  if createSplits:
-    if createSplits == True:
-      createSplits = {'train': 0.8, 'val': 0.5}
-    for splitKey in createSplits:
-      if not splitKey in SPLIT_NAMES:
-        raise ValueError(f'unkown key {splitKey} - createSplits has to be a dictionary containing the keys `train` and `val` and values between 0 and 1')
-    data = {}
-    data['source'] = read_single_txt(dataDir + 'sources.txt')
-    data['target'] = read_single_txt(dataDir + 'targets.txt')
-    entries = len(data['source'])
-    assert entries == len(data['target']), "Source and target must have the same amount of lines"
-    for textName in ['source', 'target']:
-      text = data[textName]
-      previousSplitIndex = 0
-      createSplits['test'] = 1.
-      for splitName in SPLIT_NAMES:
-        splitFraction = createSplits[splitName]
-        if not 0 <= splitFraction <= 1:  # check split values
-          raise ValueError('incorrect split sizes')
-        splitIndex = int((entries - previousSplitIndex) * splitFraction + previousSplitIndex)
-        split = text[previousSplitIndex:splitIndex]
-        write_txt('{}{}.{}'.format(dataDir, splitName, textName), split)
-        previousSplitIndex = splitIndex
-      assert previousSplitIndex == entries, f'{previousSplitIndex} != {entries}'
+def provide_data(
+        dataset_name: str,
+        tokenizer_name: str,
+        model_name: str,
+        size: Optional[int] = None,
+        create_splits: Optional[bool] = False,
+        splits2tokenize: Optional[list] = SPLIT_NAMES,
+        filtering: Optional[bool] = True):
+    """Provides tokenized data for training
 
-  # tokenizing
-  tokenizer = transformers.AutoTokenizer.from_pretrained(tokenizerName)
-  maxTokenSize = tokenizer.max_model_input_sizes[modelName]
-  if filtering:
-    filtered = '_filtered'
-  else:
-    filtered = ''
-  tensorDir = f'{dataDir}{tokenizerName}{filtered}/'
-  check_make_dir(tensorDir, True)
-  for splitName in splits2tokenize:
-    sourceText = read_single_txt('{}{}.{}'.format(dataDir, splitName, 'source'))
-    targetText = read_single_txt('{}{}.{}'.format(dataDir, splitName, 'target'))
-    textLen = len(sourceText)
-    assert textLen == len(targetText)
-    if size:  # optional limitation of samples for tokenization
-      sourceText = sourceText[:size]
-      targetText = targetText[:size]
-    log(f'tokenizing target batch for {splitName} of {textLen} samples')
+    Args:
+        dataset_name (str): foldername in datasets directory
+        tokenizer_name (str): huggingface tokenizer name (same as model name)
+        model_name (str): huggingface model name
+        size (Optional[int], optional): Limits the amount of samples that are
+        taken for tokenization for each split.
+        create_splits (Optional[bool], optional): Split the dataset into train,
+        validation and test splits.
+        Has to be provided as a dict containing the keys `train` and `val` and
+        values between 0 and 1.
+        If `True` uses a default 80/10/10 split. Defaults to False.
+        splits2tokenize (Optional[list], optional): Can be set to only
+        tokenize certain splits. Defaults to SPLIT_NAMES.
+        filtering (Optional[bool], optional): Longer examples than the
+        maximum token size are filtered,
+        else they are truncated. Defaults to True.
+
+    Raises:
+        ValueError: incorrect inputs
+        IOError: incompatible text and summary number
+    """
+    # checking input
+    if not model_name in MODEL_NAMES:
+        raise ValueError('unkown model')
+    if not tokenizer_name in TOKENIZER_NAMES:
+        raise ValueError('unkown tokenizer')
+    if size and size < 1:
+        raise ValueError('wrong size')
+    dataset_dir = f'dataProvider/datasets/{dataset_name}/'
+    assertDirExistent(dataset_name)
+
+    if create_splits:
+        if create_splits == True:
+            create_splits = {'train': 0.8, 'val': 0.5}
+        for split_key in create_splits:
+            if not split_key in SPLIT_NAMES:
+                raise ValueError(
+                    f'unkown key {split_key} - createSplits has to be a \
+                      dictionary containing the keys `train` and `val` \
+                        and values between 0 and 1')
+        data = {}
+        data['source'] = read_single_txt(dataset_dir + 'sources.txt')
+        data['target'] = read_single_txt(dataset_dir + 'targets.txt')
+        entries = len(data['source'])
+        assert entries == len(
+            data['target']), "Source and target must have the same amount of lines"
+        for text_name in ['source', 'target']:
+            text = data[text_name]
+            previous_split_index = 0
+            create_splits['test'] = 1.
+            for split_name in SPLIT_NAMES:
+                split_fraction = create_splits[split_name]
+                if not 0 <= split_fraction <= 1:  # check split values
+                    raise ValueError('incorrect split sizes')
+                split_index = int((entries - previous_split_index)
+                                  * split_fraction + previous_split_index)
+                split = text[previous_split_index:split_index]
+                write_txt('{}{}.{}'.format(
+                    dataset_dir, split_name, text_name), split)
+                previous_split_index = split_index
+            assert previous_split_index == entries, f'{previous_split_index} != {entries}'
+
+    # tokenizing
+    tokenizer = transformers.AutoTokenizer.from_pretrained(tokenizer_name)
+    max_token_size = tokenizer.max_model_input_sizes[model_name]
     if filtering:
-      targetTokens = tokenizer(targetText, padding=True)
+        filtered = '_filtered'
     else:
-      targetTokens = tokenizer(targetText, padding=True, return_tensors="pt")
-    if len(targetTokens['attention_mask'][0]) > maxTokenSize:
-      targetTokens = len(targetTokens['attention_mask'][0])
-      raise IOError(f'target contains more than {maxTokenSize} tokens: {targetTokens}')
-    log(f'tokenizing source batch for {splitName}')
-    if filtering:
-      sourceTokens = tokenizer(sourceText, padding='max_length', truncation=True, max_length=maxTokenSize + 1)
-    else:
-      sourceTokens = tokenizer(sourceText, padding='max_length', truncation=True, return_tensors='pt')
-    if filtering:  # finding tokenizations that are too long
-      tokensDeletes = []
-      for i, attention in enumerate(sourceTokens['attention_mask']):
-        if len(attention) < maxTokenSize:
-          break
-        if attention[maxTokenSize]:
-          tokensDeletes.append(i)
-      deletedSamples = len(tokensDeletes)
-      log('{} ({}%) of samples were filtered because they were too long'.format(deletedSamples, round((deletedSamples / len(sourceTokens['attention_mask'])) * 100, 2)))
-    for textName, tokens in [('source', sourceTokens), ('target', targetTokens)]:
-      if filtering:  # creating filtered PyTorch tensors from tokenization lists and replacing them
-        for key in tokens:  # tokens contains `inputs_ids` and `attention_mask`
-          tokensList = tokens[key]
-          for i in sorted(tokensDeletes, reverse=True):  # actual filtering
-            del tokensList[i]
-          tokensTensor = torch.LongTensor(np.array(tokensList)[:, :512])
-          tokens[key] = tokensTensor
-      tensorPath = f'{tensorDir}{splitName}_{textName}.pt'
-      log(f'{tensorPath} with output size:', tokens[list(tokens.keys())[0]].size())
-      assertFileInxestent(tensorPath)
-      torch.save(tokens, tensorPath)
+        filtered = ''
+    tensor_dir = f'{dataset_dir}{tokenizer_name}{filtered}/'
+    check_make_dir(tensor_dir, True)
+    for split_name in splits2tokenize:
+        source = read_single_txt('{}{}.{}'.format(
+            dataset_dir, split_name, 'source'))
+        target = read_single_txt('{}{}.{}'.format(
+            dataset_dir, split_name, 'target'))
+        text_length = len(source)
+        assert text_length == len(target)
+        if size:  # optional limitation of samples for tokenization
+            source = source[:size]
+            target = target[:size]
+        log(f'tokenizing target batch for {split_name} of {text_length} samples')
+        if filtering:
+            target_tokens = tokenizer(target, padding=True)
+        else:
+            target_tokens = tokenizer(
+                target, padding=True, return_tensors="pt")
+        if len(target_tokens['attention_mask'][0]) > max_token_size:
+            target_tokens = len(target_tokens['attention_mask'][0])
+            raise IOError(
+                f'target contains more than {max_token_size} tokens: {target_tokens}')
+        log(f'tokenizing source batch for {split_name}')
+        if filtering:
+            source_tokens = tokenizer(
+                source, padding='max_length', truncation=True, max_length=max_token_size + 1)
+        else:
+            source_tokens = tokenizer(
+                source, padding='max_length', truncation=True, return_tensors='pt')
+        if filtering:  # finding tokenizations that are too long
+            tokens_deletes = []
+            for i, attention in enumerate(source_tokens['attention_mask']):
+                if len(attention) < max_token_size:
+                    break
+                if attention[max_token_size]:
+                    tokens_deletes.append(i)
+            deleted_samples = len(tokens_deletes)
+            log('{} ({}%) of samples were filtered because they were too long'.format(
+                deleted_samples,
+                round((deleted_samples / len(source_tokens['attention_mask'])) * 100, 2))
+              )
+        for text_name, tokens in [('source', source_tokens), ('target', target_tokens)]:
+            # creating filtered PyTorch tensors from
+            # tokenization lists and replacing them
+            if filtering:
+                for key in tokens:  # tokens contains `inputs_ids` and `attention_mask`
+                    tokens_list = tokens[key]
+                    for i in sorted(tokens_deletes, reverse=True):  # actual filtering
+                        del tokens_list[i]
+                    tokens_tensor = torch.LongTensor(
+                        np.array(tokens_list)[:, :512])
+                    tokens[key] = tokens_tensor
+            tensor_path = f'{tensor_dir}{split_name}_{text_name}.pt'
+            log(f'{tensor_path} with output size:',
+                tokens[list(tokens.keys())[0]].size())
+            assertFileInxestent(tensor_path)
+            torch.save(tokens, tensor_path)
+
 
 if __name__ == "__main__":
-  fire.Fire(provideData)
+    fire.Fire(provide_data)
--- evaluator/eval.py
+++ evaluator/eval.py
@@ -1,44 +1,62 @@
 """
 orchestrate the evaluation
 """
-from modelTrainer.abstractive_summarizer import AbstractiveSummarizer
-from utilities.gerneral_io_utils import write_table
-from evaluator.metrics import SemanticSimilarityMetric, Metric
-from utilities.cleaning_utils import limit_data
-from typing import Union
+from typing import Union, Optional
 import torch
+from modelTrainer.abstractive_summarizer import AbstractiveSummarizer
+from evaluator.metrics import Metric
+from utilities.gerneral_io_utils import write_table
+from utilities.cleaning_utils import limit_data
 
-def run_evaluation(data: dict, model: AbstractiveSummarizer, metric, out_dir, samples: int, base_model: AbstractiveSummarizer = None):
-    """
-    orchestrator to add different evaluation methods
-    :param data:
-    :param model:
-    :param metric:
-    :param out_dir:
-    :param samples:
-    :param base_model:
-    :return:
+
+def run_evaluation(
+        data: dict,
+        model: AbstractiveSummarizer,
+        metric: Metric,
+        out_dir: str,
+        samples: int,
+        base_model: Optional[AbstractiveSummarizer] = None):
+    """orchestrator for the evaluation process
+
+    Args:
+        data (dict): evaluation data dictionary
+        model (AbstractiveSummarizer): model to be evaluated
+        metric (Metric): choosen metric for the evaluation
+        out_dir (str): directory to store results like evaluation excel/csv
+        samples (int): number of texts to evaluate on
+        base_model (Optional[AbstractiveSummarizer], optional): 
+        Not fine-tuned model as baseline. Defaults to None.
     """
     # limit data to evaluate
     data = limit_data(data, samples)
     # calculate different scores
     calculate_scores(data, metric, model, base_model, out_dir)
 
-def calculate_scores(data: dict, metric: Metric, model: AbstractiveSummarizer, base_model: Union[None, AbstractiveSummarizer], out_dir: str):
-    """
-    calculate all metric permutations
-    :param data:
-    :param metric:
-    :param model:
-    :param base_model:
-    :param out_dir:
-    :return:
+
+def calculate_scores(
+        data: dict,
+        metric: Metric,
+        model: AbstractiveSummarizer,
+        base_model: Union[None, AbstractiveSummarizer],
+        out_dir: str):
+    """calculate the matric scores for the fine tuned model
+       given the validation set
+
+    Args:
+        data (dict): validation set
+        metric (Metric): supported metric
+        model (AbstractiveSummarizer): fine tuned model
+        base_model (Union[None, AbstractiveSummarizer]): baseline model
+        out_dir (str): store output
     """
     # calculate predictions
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    source_texts = model.tokenizer.batch_decode(data['source']['input_ids'].to(device))
+    device = torch.device(
+        'cuda') if torch.cuda.is_available() else torch.device('cpu')
+    source_texts = model.tokenizer.batch_decode(
+        data['source']['input_ids'].to(device))
     predicted_summaries = model.predict(data['source'])
-    target_summaries = model.tokenizer.batch_decode(data['target']['input_ids'].to(device))
+    target_summaries = model.tokenizer.batch_decode(
+        data['target']['input_ids'].to(device))
 
     # only if base model given
     if base_model:
@@ -50,19 +68,26 @@ def calculate_scores(data: dict, metric: Metric, model: AbstractiveSummarizer, b
     similarity_scores = list()
 
     # calculate metrics
-    for i, (source_text, predicted_summary, target_summary) in enumerate(zip(source_texts, predicted_summaries, target_summaries)):
+    for i, (source_text, predicted_summary, target_summary) in \
+            enumerate(zip(source_texts, predicted_summaries, target_summaries)):
+
         gold_prediction_score = metric.get_score(target_summary, source_text)
-        fine_tuned_prediction_score = metric.get_score(predicted_summary, source_text)
-        summary_similarity_score = metric.get_score(predicted_summary, target_summary)
+        fine_tuned_prediction_score = metric.get_score(
+            predicted_summary, source_text)
+        summary_similarity_score = metric.get_score(
+            predicted_summary, target_summary)
 
         gold_scores.append(gold_prediction_score)
         fine_tuned_scores.append(fine_tuned_prediction_score)
         similarity_scores.append(summary_similarity_score)
 
         if base_model:
-            base_prediction_score = metric.get_score(base_predictions[i], source_text)
+            base_prediction_score = metric.get_score(
+                base_predictions[i], source_text)
             base_scores.append(base_prediction_score)
 
+    # create dict to prepare
+    # data for excel/csv output
     overview_dict = {
         "text": source_texts,
         "target_summary": target_summaries,
